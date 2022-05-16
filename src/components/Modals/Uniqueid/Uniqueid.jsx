2600575d7ea028b5bb725e2073f057be26339220
infuman69 <saharoy.soham123@gmail.com>
2022-05-16 16:02:16 +0530

    Manual Entry done(norom norom)

--- src/components/Modals/Uniqueid/Uniqueid.jsx
+++ src/components/Modals/Uniqueid/Uniqueid.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+
+
+
 import './Uniqueid.style.css'
 const Uniqueid = () => {
+  let history = useHistory()
+  const [inputVal,setInputVal]=useState('')
+  const handleChange = (e) => {
+      setInputVal(e.target.value)
+  }
+  const handleClick = (e) => {
+      e.preventDefault()
+      history.push('/'+ inputVal)
+      setInputVal('')
+  }
   return (
     <div className='uniqueid'>
         <h4>Unique Confirmation Number</h4>
-        <input type="text" name="" id="" />
+        <input type="text" name="" id="" value={inputVal} onChange={(e) => handleChange(e)}/>
         <div className="btn-uniqueid-container">
-            <button type="submit" className='btn-uniqueid'>CONFIRM</button>
+            <button type="submit" className='btn-uniqueid' onClick = {(e) => handleClick(e)}
+            >CONFIRM</button>
         </div>
     </div>
   )
